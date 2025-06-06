455487fec6f09ab6cce6542aeee2308e3b51105c
Cornie98 <conniem98@icloud.com>
2025-06-06 15:54:01 +0100

    amend the connection.js file and package.json to run seed-prod

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-const ENV = process.env.NODE_ENV || "development";
 
 const config = {};
 
@@ -12,14 +13,14 @@ if (ENV === "production") {
     config.max = 2;
 }
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+const db = new Pool(config);
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
+if (ENV !== "production" && !process.env.PGDATABASE) {
     throw new Error("No PGDATABASE configured");
 } else {
-    console.log(`Connected to ${process.env.PGDATABASE}`);
+    console.log(
+        `Connected to ${ENV === "production" ? "production database" : process.env.PGDATABASE}`
+    );
 }
 
 module.exports = db;
