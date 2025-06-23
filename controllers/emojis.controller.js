8d30b4975c8069cfa5647d7cd1a4dcb3590917de
Cornie98 <conniem98@icloud.com>
2025-06-23 09:59:32 +0100

    update endpoints and  API documentation website

--- controllers/emojis.controller.js
+++ controllers/emojis.controller.js
@@ -3,6 +3,7 @@ const { getOrAddEmojiId, addEmojiReaction } = require("../models/emojis.model");
 
 exports.postArticleEmoji = async (request, response, next) => {
     try {
+        const emojiRegex = /^\p{Emoji}$/u;
         const { article_id } = request.params;
         const { username, emoji } = request.body;
 
@@ -12,6 +13,9 @@ exports.postArticleEmoji = async (request, response, next) => {
                 .send({ msg: "missing username or emoji" });
         }
 
+        if (!emojiRegex.test(emoji)) {
+            return response.status(400).send({ msg: "invalid emoji" });
+        }
         const emoji_id = await getOrAddEmojiId(emoji);
         const reaction = await addEmojiReaction(article_id, username, emoji_id);
         response.status(201).send({ reaction });
